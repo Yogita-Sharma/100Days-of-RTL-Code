1f2e4829a1ca9a9934c6c1e2723f08347d23e3b5
Yogita-Sharma <167100992+Yogita-Sharma@users.noreply.github.com>
2024-04-17 18:32:57 +0530

    Half_Adder.v

--- Day-2 Half_adder/Half_Adder (2).v
+++ Day-2 Half_adder/Half_Adder (2).v
@@ -1,6 +1,23 @@
-module half_adder(a,b,s,c);
-input a,b;
-output s,c;
-xor s1(s,a,b);
-and c1(c,a,b);
+module half_adder(a,b,s1,c1,s2,c2,s3,c3);
+  
+    input wire a,b;
+    output reg s1,c1,s2,c2;
+    output s3,c3;
+  
+  xor X1(s1,a,b);   
+ and X2(c1,a,b);
+  
+  assign s3 = a ^ b;     
+  assign c3 = a & b;
+  
+  
+ always @(a,b)      
+    begin
+      
+      s2=a^b;
+      c2=a&b;
+      
+    end
+  
+  
 endmodule
