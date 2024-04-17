27e0baca186c697c3b6d411e7b7c43e32f55d422
Yogita-Sharma <167100992+Yogita-Sharma@users.noreply.github.com>
2024-04-17 18:34:01 +0530

    half_Adder_tb .v

--- Day-2 Half_adder/half_Adder_tb (2).v
+++ Day-2 Half_adder/half_Adder_tb (2).v
@@ -1,18 +1,30 @@
-`include "half_adder.v"
-
 module half_adder_tb;
-reg a,b;
-wire s,c;
-half_adder dut (a,b,s,c);
-
-initial begin
-    $dumpfile("half_adder.vcd");
-    $dumpvars(0,half_adder_tb);
-    a=0; b=0;
-    #1 a=0; b=1;
-    #2 a=1; b=0;
-    #4 a=1; b=1;
-
-
-end
+  reg a,b;
+  wire s1,c1,s2,c2,s3,c3;
+  
+  half_adder ha(a,b,s1,c1,s2,c2,s3,c3);
+  initial
+    begin
+      
+      $monitor("t=%0t, a=%0b, b=%0b, s1=%0b, c1=%0b, s2=%0b, s3=%0b, c3=%0b",                     $time, a,b,s1,c1,s2,c2,s3,c3);
+      
+      a=0; b=0;
+      #4
+      a=0; b=1;
+      #4
+      a=1; b=0;
+      #4
+      a=1; b=1;
+      #4
+      $finish();
+      
+       end
+  
+  initial 
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars();
+    end
+  
 endmodule
+
